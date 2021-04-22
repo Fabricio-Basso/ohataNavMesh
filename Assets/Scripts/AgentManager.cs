6aeb0d911599ed143e57988beca599656943d392
fabricio-basso <fabricio.basso12@gmail.com>
2021-04-21 21:19:16 -0300

    first commit

---
+++ Assets/Scripts/AgentManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+    
+    void Start()
+    {
+        //Encontrando todos os GO com essa tag "ai"
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    void Update()
+    {
+        //Pega a posição do mouse e seta como o destino
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                foreach (GameObject a in agents)
+                {
+                    a.GetComponent<IA_Control>().agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/IA_Control.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA_Control : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+}
